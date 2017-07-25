b4ccb567b022a50b333937d7befcbe7d16fab1a1
Mason <lantay@alumni.unc.edu>
2017-07-25 15:30:39 -0700

    configured webpack to work with css as seen at vimeo 7:30

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
             presets: ['react', 'es2015']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+
       }
     ]
   },
