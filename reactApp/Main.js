38812a959a2411464a8953335b25a271955837d7
Mason <lantay@alumni.unc.edu>
2017-07-25 16:05:08 -0700

    editor is there

--- reactApp/Main.js
+++ reactApp/Main.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import {
+  Editor,
+  EditorState
+} from 'draft-js';
+
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
   }
 
+  onChange(editorState) {
+    this.setState({
+      editorState
+    });
+  }
   render() {
     return (
       <div >
         Testing
+        <h1>Editor</h1>
+        <Editor
+          onChange= {this.onChange.bind(this)}
+          editorState={this.state.editorState}
+        />
       </div >
     );
   }
